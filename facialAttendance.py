0390181e8ccb28bbe7149f3d3ef7ebe8b28cbc87
SenyoGela <senogelamariconb@gmail.com>
2023-02-18 03:11:19 +0800

    create variable for the images

--- facialAttendance.py
+++ facialAttendance.py
@@ -8,3 +8,14 @@ from datetime import datetime
 
 video_capture = cv2.VideoCapture(0)
 
+wang_image = face_recognition.load_image_file("photos/dylan.jpg")
+wang_encoding = face_recognition.face_encoding(wang_image)[0]
+
+chen_image = face_recognition.load_image_file("photos/xingxu.jpg")
+chen_encoding = face_recognition.face_encoding(chen_image)[0]
+
+hong_image = face_recognition.load_image_file("photos/joshua.jpg")
+hong_encoding = face_recognition.face_encoding(hong_image)[0]
+
+zhao_image = face_recognition.load_image_file("photos/lusi.jpg")
+zhao_encoding = face_recognition.face_encoding(zhao_image)[0]
