4d678b496e7bcd754639a59ea60559ecdfaf32fe
SenyoGela <senogelamariconb@gmail.com>
2023-02-18 03:45:01 +0800

    show output

--- facialAttendance.py
+++ facialAttendance.py
@@ -70,3 +70,6 @@ while True:
                     print(students)
                     current_time = now.strftime("%H-%M-%S")
                     lnwriter.writerow([name, current_time])
+    cv2.imshow("attendance system",frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
