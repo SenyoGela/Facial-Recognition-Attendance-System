666a1ca2bc7e2bdcb95680dcea68115d921732df
SenyoGela <senogelamariconb@gmail.com>
2023-02-18 03:29:18 +0800

    extract video data

--- facialAttendance.py
+++ facialAttendance.py
@@ -42,9 +42,12 @@ face_names = []
 s = True
 
 now = datetime.now()
-current_date = now.strftime("%Y-%m-%d"
+current_date = now.strftime("%Y-%m-%d")
 
 f = open(current_date+'.csv','w+',newline='')
 lnwriter = csv.writer(f)
 
-
+while True:
+    _,frame = video_capture.read()
+    small_frame = cv2.resize(frame,(0,0),fx=0.25,fy=0.25)
+    rgb_small_frame = small_frame[:,:,::-1]
